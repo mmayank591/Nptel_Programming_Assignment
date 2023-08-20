4b1a0f398b43055a672ec7c8b0e17e6d001f575e
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-08-20 23:29:19 +0530

    Solve Question4_2.java

---
+++ Question4_2.java
@@ -0,0 +1,15 @@
+// The following is the declaration of the main class named Question42
+public class Question42 { 
+	public static void main(String args[]){
+		int year; // integer type variable to store year
+// Create an object of Calendar class. 
+
+		// Use getInstance() method to initialize the Calendar object.
+	
+		// Initialize the int variable year with the current year
+java.util.Calendar current;
+current = java.util.Calendar.getInstance();
+year=current.get(current.YEAR);
+// Print the current Year
+		System.out.println("Current Year: "+year);
+}}
