55da27fe9e5da493d14930eeed48fd803039f3e6
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-08-30 21:05:56 +0530

    Solve Excerise6_1.java

---
+++ Excerise6_1.java
@@ -0,0 +1,6 @@
+// Write the appropriate code to extend the Thread class to complete the class Question61.
+public class Question61 extends Thread{
+  public void run()
+  {
+    System.out.print("Thread is Running.");
+  }
